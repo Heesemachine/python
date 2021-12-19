c09457ae9eaa51cb8219d111a54984ca28c2ccbc
Heesemachine <93719680+Heesemachine@users.noreply.github.com>
2021-12-19 18:14:58 +0200

    Update l7z1.py

--- l7z1.py
+++ l7z1.py
@@ -8,8 +8,9 @@ c=int(input('Vvedit k-st stovbciv matricu: '))
 for i in range(r):
     A.append([int(input('{0} {1} el:'.format(i,j)))for j in range(c)])
 for i in range(len(A)):
-    for j in range(len(A[i])):
-        if A[i][j]<0 and (i+1)%2==0 and (j+1)%2==0:
-            suma +=1
+    for j in range(len(A)):
+        if i%2 == 0 and j%2 == 0:
+            if A[i][j] < 0:
+                suma +=1
 print(A)
 print(suma)
