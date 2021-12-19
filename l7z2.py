5e1ba410ddaf188dd47ee9bb8bf633ad62706dad
Heesemachine <93719680+Heesemachine@users.noreply.github.com>
2021-12-19 18:21:28 +0200

    Update l7z2.py

--- l7z2.py
+++ l7z2.py
@@ -1,17 +1,10 @@
-"""
-Побудувати прямокутну матрицю А, елементи якої задаються формулою:
-a_ij = 1 / (i+j)
-Обчислити суму додатних елементів елементів матриці А.
-"""
-A = []
-sum_d = 0
-b = int(input('Рядки: '))
-c = int(input('Стовпці: '))
-for i,j in range(b,c):
-    A.append([1 / ((i + j)) for j in range(c)])
-print(A)
-for i in range(b):
-    for j in range(c):
-        if A[i][j] < 0 and (i + 1) % 2 == 0 and (j + 1) % 2 == 0:
-            sum_d += 1
-print("Сума відємних елементів матриці з парною сумою індексів : {0:.2f}".format(sum_d))
+n = int(input("Рядки : "))
+m = int(input("Стовпці : "))
+suma = 0
+a = [[(1/(i+j)) for j in range(1,m)] for i in range(1,n)]
+print(*a, sep="\n")
+for i in range(len(a)):
+    for j in range(len(a)):
+        if (i+j)%2 == 1:
+            suma += a[i][j]
+print("Сума відємних елементів матриці з парною сумою індексів : {0:.2f}".format(suma))
