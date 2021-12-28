5deeeda5cc9612e33053a7901e7fc602302b7d8c
Heesemachine <93719680+Heesemachine@users.noreply.github.com>
2021-12-28 20:56:12 +0200

    Update l10z2.py

--- l10z2.py
+++ l10z2.py
@@ -23,9 +23,17 @@ class audiofile():
         self.glybina_cod = glybina_cod
     def age(self):
         return get_today_date().year - self.data_st.year
-    data_st=date(2019, 5, 16)
+
+    def dots_for_save(self):
+        return self.truvalist * self.chas_d
+
+    def vaga_fily(self):
+        return 
+
+data_st = date(2021, 5, 1)
 AudioFile = audiofile(input('Формат Файлу: '), input('Дата створення: '), input('Тривалість запису: '),
 float(input('Частота Дискритизації: ')), input('Глубина кодування: '))
 print(" Формат файлу: {0}\n Дата створення: {1}\n Тривалість запису: {2}\n Частота Дискритизації: {3:.2f}\n Глубина Кодування {4}\n"
       .format(AudioFile.formatfily, AudioFile.data_st, AudioFile.truvalist, AudioFile.chas_d, AudioFile.glybina_cod))
+
 
